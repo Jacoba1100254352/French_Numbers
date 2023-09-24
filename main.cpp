f877f6a3c54ef90ebf861697f22075e1b9804911
Jacob Anderson <jacobdanderson@gmail.com>
2023-09-24 00:46:36 -0600

    Alignment

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@ using namespace std;
 using namespace literals::chrono_literals;
 
 ///   General Functions   ///
-unsigned long long RandomOption();
-unsigned long long RandomPathOption();
+unsigned long long RandomOption(double maxVal);
 unsigned StartingScript( unsigned& );
 //unsigned Timer();
 struct Timer
@@ -66,9 +65,7 @@ unsigned hintsGiven;
 *   Main   *
 ************/
 
-
-int main()
-{
+int main() {
     ///   Initialize Variables   ///
     enum PracticeForm { WritToNum = 1, NumToWrit = 2, DaysMonths = 3, Random = 4 };
     unsigned percentCorrect, desiredPracticeForm, randomOption, userRandMax;
@@ -79,48 +76,46 @@ int main()
     time["total"] = timer.endTimer();
 
     ///   Welcome the User   ///
-    StartingScript( desiredPracticeForm );
+    StartingScript(desiredPracticeForm);
     userRandMax = rangeMax;
 
     ///   Global Variables   ///
-    correctAnswers = 0, incorrectAnswers = 0, hintsGiven = 0;
+    correctAnswers = incorrectAnswers = hintsGiven = 0;
 
     //chrono::time_point<chrono::steady_clock> times = chrono::high_resolution_clock::now();
     //time["total"] = times;
     do {
         if (desiredPracticeForm == Random)
-            randomOption = RandomPathOption();
+            randomOption = RandomOption(3);
 
         // Continues rangeMax hardcoding for the Days and the Months option (see note in StartingScript function)
-        if (randomOption == 3)
-            rangeMax = 19;
-        else rangeMax = userRandMax;
+        rangeMax = (randomOption == 3) ? 19 : userRandMax;
 
         if (desiredPracticeForm == WritToNum || randomOption == 1)
             do WrittenToNum(practiceEnd);
-            while (!practiceEnd && desiredPracticeForm != Random );
+            while (!practiceEnd && desiredPracticeForm != Random);
         else if (desiredPracticeForm == NumToWrit || randomOption == 2)
             do NumToWritten(practiceEnd);
-            while (!practiceEnd && desiredPracticeForm != Random );
+            while (!practiceEnd && desiredPracticeForm != Random);
         else if (desiredPracticeForm == DaysMonths || randomOption == 3)
             do DaysAndMonths(practiceEnd);
-            while (!practiceEnd && desiredPracticeForm != Random );
+            while (!practiceEnd && desiredPracticeForm != Random);
         else throw invalid_argument("Error: Invalid practice form passed");
     } while (desiredPracticeForm == Random && !practiceEnd);
 
     //chrono::duration<float> totalDuration = end - start;
 
-    percentCorrect = (correctAnswers*100)/(correctAnswers + incorrectAnswers);
+    percentCorrect = (correctAnswers * 100) / (correctAnswers + incorrectAnswers);
 
     cout << "Congrats! You finished." << endl
          << "Incorrect Answers: " << incorrectAnswers << endl
          << "Correct Answers: " << correctAnswers << endl;
     if (hintsGiven > 0)
         cout << "Hints Given: " << hintsGiven << endl;
-    cout << "Percent Correct: " << percentCorrect << endl;
-    cout << "Time taken on the correct questions: " << time["correct"] << "s " << endl;
-    cout << "Time taken on the incorrect questions: " << time["incorrect"] << "s " << endl;
-    cout << "Total duration: " << time["total"] << "s " << endl;
+    cout << "Percent Correct: " << percentCorrect << endl
+         << "Time taken on the correct questions: " << time["correct"] << "s " << endl
+         << "Time taken on the incorrect questions: " << time["incorrect"] << "s " << endl
+         << "Total duration: " << time["total"] << "s " << endl;
 
     return 0;
 }
@@ -129,22 +124,12 @@ int main()
 /************************
 *   General Functions   *
 ************************/
-unsigned long long RandomOption()
+unsigned long long RandomOption(double maxVal = static_cast<double>(rangeMax))
 {
     ///   Use random library to effectively randomize the dice rolls   ///
     random_device rd;
     mt19937 mt(rd());
-    uniform_real_distribution<> randomDistribution(1, static_cast<double>(rangeMax) + 1); // Add one to make it inclusive
-
-    return static_cast<unsigned long long>(randomDistribution(mt));
-}
-
-unsigned long long RandomPathOption()
-{
-    ///   Use random library to effectively randomize the dice rolls   ///
-    random_device rd;
-    mt19937 mt(rd());
-    uniform_real_distribution<> randomDistribution(1, 4); // Add one to make it inclusive
+    uniform_real_distribution<> randomDistribution(1, maxVal + 1); // Add one to make it inclusive
 
     return static_cast<unsigned long long>(randomDistribution(mt));
 }
@@ -209,49 +194,42 @@ unsigned StartingScript(unsigned& desiredPracticeForm)
 /*****************************************
 *   Days and Months Practice Functions   *
 *****************************************/
-void DaysAndMonths( bool& practiceEnd )
-{
+
+void DaysAndMonths(bool &practiceEnd) {
+    string DaysAndMonthsFrench[] = {"", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche",
+                                    "janvier", "février", "mars", "avril", "mai", "juin", "juillet",
+                                    "août", "septembre", "octobre", "novembre", "décembre"};
+
+    string DaysAndMonthsEnglish[] = {"", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
+                                     "January", "February", "March", "April", "May", "June", "July",
+                                     "August", "September", "October", "November", "December"};
+
     //Timer timer;
+    unsigned long long randomNumber = RandomOption();
+    string answer = DaysAndMonthsFrench[randomNumber];
+    string prompt = DaysAndMonthsEnglish[randomNumber];
     string userAnswer;
-    unsigned long long randomNumber = RandomOption(); // Assign it each time to provide random numbers
 
-    string DaysAndMonthsFrench[] = { "", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche",
-                                      "janvier", "février", "mars", "avril", "mai", "juin", "juillet",
-                                      "août", "septembre", "octobre", "novembre", "décembre"}; // position 0 is blank because of the random option hard-coding
-
-    string DaysAndMonthsEnglish[] = { "", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
-                                      "January", "February", "March", "April", "May", "June", "July",
-                                      "August", "September", "October", "November", "December"};// position 0 is blank because of the random option hard-coding
-
-    do
-    {
-        cout << "\n" << DaysAndMonthsEnglish[randomNumber] << endl;
+    do {
+        cout << prompt << endl;
         userAnswer = UserInputWritten();
 
-        while ((userAnswer != DaysAndMonthsFrench[randomNumber]) && (userAnswer != "0") && (userAnswer != "?")) {
-            incorrectAnswers++;
-            cout << "Incorrect Answer, Please try again" << "   Hint: Make sure your capitalization is right"
-                 << endl;
-            cout << DaysAndMonthsEnglish[randomNumber] << endl;
-            userAnswer = UserInputWritten();
-        }
-
-        if (userAnswer == "0")
+        if (userAnswer == "0") {
             practiceEnd = true;
-        else if (userAnswer == "?")
-        {
-            cout << "The Answer is: " << DaysAndMonthsFrench[randomNumber] << endl;
+            break;
+        } else if (userAnswer == "?") {
+            cout << "The Answer is: " << answer << endl;
             hintsGiven++;
-        }
-        else if (userAnswer == DaysAndMonthsFrench[randomNumber]) {
-            cout << "Correct!" << "\n" << endl;
+        } else if (userAnswer == answer) {
+            cout << "Correct!\n" << endl;
             correctAnswers++;
-            //system("clear");
+        } else {
+            cout << "Incorrect Answer, Please try again. Hint: Make sure your capitalization is right" << endl;
+            incorrectAnswers++;
         }
-        else throw invalid_argument("Error: Invalid Argument was passed"); // Theoretically this should never be thrown
-
-    } while (userAnswer == "?");
+    } while (userAnswer == "?" || userAnswer != answer);
 }
+
 
 
 /*******************************
@@ -281,25 +259,27 @@ string UserInputWritten() {
 
 void NumToWritten(bool& practiceEnd) {
     unsigned long long randomNumber = RandomOption();
+    string answer = WrittenFrenchNumbers(randomNumber);
     string userAnswer;
 
     do {
         cout << randomNumber << endl;
         userAnswer = UserInputWritten();
 
-        if (userAnswer == "0")
+        if (userAnswer == "0") {
             practiceEnd = true;
-        else if (userAnswer == "?") {
-            cout << "The Answer is: " << WrittenFrenchNumbers(randomNumber) << endl;
+            break;
+        } else if (userAnswer == "?") {
+            cout << "The Answer is: " << answer << endl;
             hintsGiven++;
-        } else if (userAnswer == WrittenFrenchNumbers(randomNumber)) {
+        } else if (userAnswer == answer) {
             cout << "Correct!" << endl << endl;
             correctAnswers++;
         } else {
+            cout << "Incorrect Answer, Please try again" << "   Hint: Remember to use \"-\" in between each word" << endl;
             incorrectAnswers++;
-            cout << "Incorrect Answer, Please try again" << "   Hint: Remember to use \"-\" in between each word" << endl << randomNumber << endl;
         }
-    } while (userAnswer == "?");
+    } while (userAnswer == "?" || userAnswer != answer);
 }
 
 
@@ -307,36 +287,45 @@ void NumToWritten(bool& practiceEnd) {
 *   Enumerated Number Functions   *
 **********************************/
 
+// UserInputEnumerated function
 unsigned long long UserInputEnumerated() {
     const unsigned ASCII_Q_Mark = 00101111;
     string userAnswer;
 
     while (true) {
+        // Input user decision
         cout << "Enter Answer: ";
         cin >> userAnswer;
         cout << endl;
 
+        // Failsafe for correct user input
         try {
-            if (userAnswer != "?")
+            if (userAnswer != "?") {
                 return stoi(userAnswer);
-        } catch (...) { cout << "Invalid input, please try again." << endl; }
+            }
+        } catch (...) {
+            cout << "Invalid input, please try again." << endl;
+        }
 
-        if (userAnswer == "?")
+        if (userAnswer == "?") {
             return ASCII_Q_Mark;
+        }
     }
 }
 
-void WrittenToNum(bool& practiceEnd) {
+// WrittenToNum function
+void WrittenToNum(bool &practiceEnd) {
     const unsigned ASCII_Q_Mark = 00101111;
     unsigned long long userAnswer = 0;
     unsigned long long randomNumber = RandomOption();
 
     do {
-        cout << '\n' << WrittenFrenchNumbers(randomNumber) << endl;
+        cout << "\n" << WrittenFrenchNumbers(randomNumber) << endl;
         userAnswer = UserInputEnumerated();
 
         if (!userAnswer) {
             practiceEnd = true;
+            
         } else if (userAnswer == ASCII_Q_Mark) {
             cout << "The Answer is: " << randomNumber << endl;
             hintsGiven++;
@@ -348,62 +337,58 @@ void WrittenToNum(bool& practiceEnd) {
                 userAnswer = UserInputEnumerated();
             }
 
-            cout << "Correct!" << '\n' << endl;
+            cout << "Correct!\n" << endl;
             correctAnswers++;
         }
     } while (userAnswer == ASCII_Q_Mark);
 }
 
 
+
 /****************************
 *   Calculation Functions   *
 ****************************/
 
-string WrittenFrenchNumbers(unsigned long long number)
-{
+string WrittenFrenchNumbers(unsigned long long number) {
     const unsigned ONE_BILLION = 1000000000;
     const unsigned ONE_MILLION = 1000000;
     const unsigned ONE_THOUSAND = 1000;
-    unsigned long long tempNumber = 0;
 
     string WritNumbers;
-    string Writ100to1Billion[5] = { "cent-", "cents-", "mille-", "millon-", "millard-" };
+    string Writ100to1Billion[5] = {"cent-", "cents-", "mille-", "millon-", "millard-"};
 
-    while (number)
-    {
-        if (number >= ONE_BILLION)
-        {
-            tempNumber = number / ONE_BILLION;
-            WrittenFrenchNumHundredsPlace(WritNumbers, tempNumber);
-            WritNumbers += Writ100to1Billion[4];
-            number %= ONE_BILLION;
-        }
-        else if (number >= ONE_MILLION)
-        {
-            tempNumber = number / ONE_MILLION;
-            WrittenFrenchNumHundredsPlace(WritNumbers, tempNumber);
-            WritNumbers += Writ100to1Billion[3];
-            number %= ONE_MILLION;
-        }
-        else if (number >= ONE_THOUSAND)
-        {
-            tempNumber = number / ONE_THOUSAND;
-            if (tempNumber != 1)
-                WrittenFrenchNumHundredsPlace(WritNumbers, tempNumber);
-            WritNumbers += Writ100to1Billion[2];
-            number %= ONE_THOUSAND;
-        }
-        else
-        {
-            WrittenFrenchNumHundredsPlace(WritNumbers, number);
-            number = 0;
-        }
+    // Process billions
+    if (number >= ONE_BILLION) {
+        unsigned long long tempNumber = number / ONE_BILLION;
+        WrittenFrenchNumHundredsPlace(WritNumbers, tempNumber);
+        WritNumbers += Writ100to1Billion[4];
+        number %= ONE_BILLION;
     }
 
-    if (WritNumbers.empty())
-        WritNumbers = "zéro-";
+    // Process millions
+    if (number >= ONE_MILLION) {
+        unsigned long long tempNumber = number / ONE_MILLION;
+        WrittenFrenchNumHundredsPlace(WritNumbers, tempNumber);
+        WritNumbers += Writ100to1Billion[3];
+        number %= ONE_MILLION;
+    }
 
-    if (WritNumbers.back() == '-')
+    // Process thousands
+    if (number >= ONE_THOUSAND) {
+        if (number != ONE_THOUSAND)
+            WrittenFrenchNumHundredsPlace(WritNumbers, number / ONE_THOUSAND);
+        WritNumbers += Writ100to1Billion[2];
+        number %= ONE_THOUSAND;
+    }
+
+    // Process remaining numbers (0 to 999)
+    WrittenFrenchNumHundredsPlace(WritNumbers, number);
+
+    // If the result is empty, the number is zero
+    if (WritNumbers.empty())
+        WritNumbers = "zéro";
+        // Remove the trailing hyphen, if any
+    else if (WritNumbers.back() == '-')
         WritNumbers.pop_back();
 
     return WritNumbers;
@@ -411,69 +396,45 @@ string WrittenFrenchNumbers(unsigned long long number)
 
 void WrittenFrenchNum0to99(string& WritNumbers, unsigned long long& number) {
     string Writ0to9[10] = {"zéro-", "un-", "deux-", "trois-", "quatre-", "cinq-", "six-", "sept-", "huit-", "neuf-"};
-    string Writ11to19[10] = {"", "onze-", "douze-", "treize-", "quatorze-", "quinze-", "seize-", "dix-sept-", "dix-huit-", "dix-neuf-"}; // [0] is empty so there is less confusion later
-    string Writ10to90[10] = {"", "dix-", "vingt-", "trente-", "quarante-", "cinquante-", "soixante-", "soixante-", "quatre-vingt-", "quatre-vingt-"}; // [0] is empty so there is less confusion later
+    string Writ11to19[10] = {"", "onze-", "douze-", "treize-", "quatorze-", "quinze-", "seize-", "dix-sept-", "dix-huit-", "dix-neuf-"};
+    string Writ10to90[10] = {"", "dix-", "vingt-", "trente-", "quarante-", "cinquante-", "soixante-", "soixante-", "quatre-vingt-", "quatre-vingt-"};
 
-    if (number >= 10 && number <= 99) { // 10 to 99
-        if ((number >= 11 && number <= 19) || (number >= 70 && number <= 79) || (number >= 90 && number <= 99)) { // 11-19 and 71-79 or 91-99
+    if (number < 10) { // 1 to 9
+        WritNumbers += Writ0to9[number];
+    } else { // 10 to 99
+        unsigned long long tens = number / 10;
+        uint8_t ones = number % 10;
+
+        if ((11 <= number && number <= 19) || (70 <= number && number <= 79) || (90 <=number && number <= 99)) {
             if (number >= 70 && number <= 79 || number >= 90 && number <= 99)
-                for (unsigned i = 1; i <= 9; i++)
-                    if (i == (number / 10))
-                        WritNumbers += Writ10to90[i];
-
-            for (int i = 1; i < 10; i++)
-                if ((number % 10) == i)
-                    WritNumbers += Writ11to19[i];
-
-            number = 0; // End the loop
-
-            ///   Write Ten's Place   ///
-        } else if (number == 10 || number >= 20) {
-            for (unsigned i = 1; i < 10; i++)
-                if (i == (number / 10))
-                    WritNumbers += Writ10to90[i];
-
-            ///   Add "et-" to numbers 21-71 that have "1"   ///
-            if (number > 20 && number <= 71 && (number % 10) == 1) // 21-71 add et- to work
+                WritNumbers += Writ10to90[tens];
+            WritNumbers += Writ11to19[ones];
+        } else if (!ones) { // TENS: 10, 20, 30, ..., 90
+            WritNumbers += Writ10to90[tens];
+        } else { ///   Add "et-" to numbers 21-71 that have "1"   ///
+            WritNumbers += Writ10to90[tens];
+            if (ones == 1 && 20 < number && number <= 71)
                 WritNumbers += "et-";
-            number %= 10; // Modulate down
-            if (number > 0) // 1 to 9
-                for (unsigned i = 1; i < 10; i++)
-                    if ((number % 10) == i)
-                        WritNumbers += Writ0to9[i];
-            number = 0; // End the loop
-        } else {
-            number %= 10; // Modulate down
-            if (number > 0) // 1 to 9
-                for (unsigned i = 1; i < 10; i++)
-                    if ((number % 10) == i)
-                        WritNumbers += Writ0to9[i];
-            number = 0; // End the loop
+            WritNumbers += Writ0to9[ones];
         }
-    } else { // 1 to 9
-        for (int i = 1; i < 10; i++)
-            if ((number % 10) == i)
-                WritNumbers += Writ0to9[i];
-        number = 0; // End the loop
     }
+    number = 0;
 }
+
 void WrittenFrenchNumHundredsPlace(string& WritNumbers, unsigned long long number)
 {
     const unsigned ONE_HUNDRED = 100;
-    unsigned long long tempNumber = 0;
 
     if (number >= ONE_HUNDRED)
     {
-        if (number >= 200) // avoids "un-cent-..." which is not a number
-        {
-            tempNumber = number / ONE_HUNDRED; // of 903, make it 9
+        unsigned long long tempNumber = number / ONE_HUNDRED;
+        if (tempNumber > 1) // avoids "un-cent-..." which is not a number
             WrittenFrenchNum0to99(WritNumbers, tempNumber);
-        }
 
         ///   Add Hundred's prefix   ///
-        if (number / ONE_HUNDRED == 1 || number % ONE_HUNDRED >= 1)
+        if (tempNumber == 1 || number % ONE_HUNDRED >= 1)
             WritNumbers += "cent-";
-        else if (number % ONE_HUNDRED == 0)
+        else if (!(number % ONE_HUNDRED))
             WritNumbers += "cents-";
     }
 
@@ -481,3 +442,4 @@ void WrittenFrenchNumHundredsPlace(string& WritNumbers, unsigned long long numbe
     number %= ONE_HUNDRED;
     WrittenFrenchNum0to99(WritNumbers, number);
 }
+
